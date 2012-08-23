de7511e5d9310d89c7ab7ac4ffc9045317647a7c
Jamon Holmgren <jamon@clearsightstudio.com>
2012-08-23 09:19:01 -0700

    Temp - 2

--- lib/motion-table/grouped_table.rb
+++ lib/motion-table/grouped_table.rb
@@ -1,55 +1,55 @@
-# module MotionTable
-#   module GroupedTable
-#     def createTableViewFromData(data)
-#       @mt_table_view_groups = data
-#       return tableView
-#     end
+module MotionTable
+  module GroupedTable
+    # def createTableViewFromData(data)
+    #   @mt_table_view_groups = data
+    #   return tableView
+    # end
 
-#     def numberOfSectionsInTableView(tableView)
-#       return @mt_table_view_groups.length
-#     end
+    # def numberOfSectionsInTableView(tableView)
+    #   return @mt_table_view_groups.length
+    # end
 
-#     # Number of cells
-#     def tableView(tableView, numberOfRowsInSection:section)
-#       return sectionAtIndex(section)[:cells].length
-#     end
+    # # Number of cells
+    # def tableView(tableView, numberOfRowsInSection:section)
+    #   return sectionAtIndex(section)[:cells].length
+    # end
 
-#     def tableView(tableView, titleForHeaderInSection:section)
-#       return sectionAtIndex(section)[:title]
-#     end
+    # def tableView(tableView, titleForHeaderInSection:section)
+    #   return sectionAtIndex(section)[:title]
+    # end
 
-#     def tableView(tableView, cellForRowAtIndexPath:indexPath)
-#       cellIdentifier = "Cell"
+    # def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    #   cellIdentifier = "Cell"
 
-#       cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
-#       cell ||= UITableViewCell.alloc.initWithFrame(CGRectZero, reuseIdentifier:cellIdentifier)
+    #   cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+    #   cell ||= UITableViewCell.alloc.initWithFrame(CGRectZero, reuseIdentifier:cellIdentifier)
 
-#       row = cellAtSectionAndIndex(indexPath.section, indexPath.row)
-#       cell.text = row[:title]
-#       return cell
-#     end
+    #   row = cellAtSectionAndIndex(indexPath.section, indexPath.row)
+    #   cell.text = row[:title]
+    #   return cell
+    # end
 
-#     def sectionAtIndex(index)
-#       @mt_table_view_groups.at(index)
-#     end
+    # def sectionAtIndex(index)
+    #   @mt_table_view_groups.at(index)
+    # end
 
-#     def cellAtSectionAndIndex(section, index)
-#       return sectionAtIndex(section)[:cells].at(index)
-#     end
+    # def cellAtSectionAndIndex(section, index)
+    #   return sectionAtIndex(section)[:cells].at(index)
+    # end
 
-#     def tableView(tableView, didSelectRowAtIndexPath:indexPath)
-#       cell = cellAtSectionAndIndex(indexPath.section, indexPath.row)
-#       tableView.deselectRowAtIndexPath(indexPath, animated: true);
-#       if self.respond_to?(cell[:action])
-#         self.send(cell[:action])
-#       end
-#     end
+    # def tableView(tableView, didSelectRowAtIndexPath:indexPath)
+    #   cell = cellAtSectionAndIndex(indexPath.section, indexPath.row)
+    #   tableView.deselectRowAtIndexPath(indexPath, animated: true);
+    #   if self.respond_to?(cell[:action])
+    #     self.send(cell[:action])
+    #   end
+    # end
 
-#     def tableView
-#       @tableView ||= UITableView.alloc.initWithFrame(self.view.frame, style:UITableViewStyleGrouped)
-#       @tableView.dataSource = self;
-#       @tableView.delegate = self;
-#       return @tableView
-#     end
-#   end
-# end
+    # def tableView
+    #   @tableView ||= UITableView.alloc.initWithFrame(self.view.frame, style:UITableViewStyleGrouped)
+    #   @tableView.dataSource = self;
+    #   @tableView.delegate = self;
+    #   return @tableView
+    # end
+  end
+end
