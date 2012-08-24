09f228ab35c6fc5bfe1d00ac85f797a827d36a0b
Silas J. Matson <silas@clearsightstudio.com>
2012-08-23 19:15:00 -0700

    cc

--- lib/motion-table/grouped_table.rb
+++ lib/motion-table/grouped_table.rb
@@ -43,7 +43,7 @@ module MotionTable
       if self.respond_to?(cell[:action])
         self.send(cell[:action])
       else
-        MotionTable::Console.log(self, actionNotImplemented: cell[:action]) unless DEBUG == false
+        MotionTable::Console.log(self, actionNotImplemented: cell[:action])
         # puts "\e[0;31m MotionTable -- #{self.to_s} does not implement the action: :#{cell[:action]} \e[0m"
       end
     end
