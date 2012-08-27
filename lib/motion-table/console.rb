fd205c8be1f03008bebc4f717762ffb8e2905c3c
Silas J. Matson <silas@clearsightstudio.com>
2012-08-27 13:35:31 -0700

    Added colors.

--- lib/motion-table/console.rb
+++ lib/motion-table/console.rb
@@ -3,6 +3,8 @@ module MotionTable
     NAME = "MotionTable::Console"
     DEFAULT_COLOR = [ '', '' ]
     RED_COLOR = [ "\e[0;31m", "\e[0m" ] # Must be in double quotes
+    GREEN_COLOR = [ "\e[0;32m", "\e[0m" ] 
+    PURPLE_COLOR = [ "\e[0;35m", "\e[0m" ] 
 
     class << self
       def log(obj, actionNotImplemented:action)
